5832583001e2e7b714d5caef3a97a214b1fad165
NLyapin <LyapinNR@rambler.ru>
2016-02-09 20:38:11 +0300

    Internatiolization

---
+++ SignalR - MemoryGame/Demo2/GameHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace Demo2
+{
+    public class GameHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello(DateTime.Now.ToString("T"));
+        }
+    }
+}
